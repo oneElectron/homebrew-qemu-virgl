49f6de04ed1e84dca5cfb62cb95365b2bcb5b3f3
oneElectron <one.electron109@protonmail.com>
2026-02-15 15:10:50 +0100

    update all depends on urls

--- Formula/virglrenderer.rb
+++ Formula/virglrenderer.rb
@@ -8,8 +8,8 @@ class Virglrenderer < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "knazarov/qemu-virgl/libangle"
-  depends_on "knazarov/qemu-virgl/libepoxy-angle"
+  depends_on "oneElectron/qemu-virgl/libangle"
+  depends_on "oneElectron/qemu-virgl/libepoxy-angle"
 
   # waiting for upstreaming of https://github.com/akihikodaki/virglrenderer/tree/macos
   patch :p1 do
